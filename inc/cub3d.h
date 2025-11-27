e0f43a972ec9e8a72646c0b6d20b11ca76f015f8
kaan-erk <kaanerktoraman@gmail.com>
2025-11-27 16:18:22 +0300

    New structs

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ktoraman < ktoraman@student.42istanbul.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 14:42:41 by ktoraman          #+#    #+#             */
-/*   Updated: 2025/11/26 17:40:19 by ktoraman         ###   ########.fr       */
+/*   Updated: 2025/11/27 16:17:37 by ktoraman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,35 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+
+typedef struct	s_texture
+{
+	char	*north;
+	char	*south;
+	char	*east;
+	char	*west;
+	int		n_flag;
+	int		s_flag;
+	int		e_flag;
+	int		w_flag;
+	char	*f;
+	char	*c;
+}				t_texture;
+
+typedef struct	s_game
+{
+	char	**map;
+	char	*map_str;
+	int		player_c;
+	char	player;
+}				t_game;
+
+
+typedef struct	s_cub
+{
+	t_texture	texture;
+	t_game		game;
+}				t_cub;
 
 void	map_checker(char *name);
 void	parse(char **av);
