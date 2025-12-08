bf658cdf31ba5438c627e2c75d262b46b53097e8
kaan-erk <kaanerktoraman@gmail.com>
2025-12-08 17:19:37 +0300

    Character check imp done

--- parse/map.c
+++ parse/map.c
@@ -6,7 +6,7 @@
 /*   By: ktoraman < ktoraman@student.42istanbul.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 20:11:13 by ktoraman          #+#    #+#             */
-/*   Updated: 2025/12/08 17:12:47 by ktoraman         ###   ########.fr       */
+/*   Updated: 2025/12/08 17:18:46 by ktoraman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,20 @@ static void	character_check(t_cub *cub)
 		{
 			cub->game.player = cub->game.real_map_str[i];
 			cub->game.player_c = 1;
+			i++;
+			break ;
 		}
 		i++;
 	}
-	if (cub->game.player_c != 0)
-		exit_free_cub("Error: Invalid character!", 1, cub);
+	while (cub->game.real_map_str[i])
+	{
+		if (cub->game.real_map_str[i] == 'N' || cub->game.real_map_str[i] == 'W'
+			|| cub->game.real_map_str[i] == 'E' || cub->game.real_map_str[i] == 'S')
+			exit_free_cub("Error: More than one player!", 1, cub);
+		i++;
+	}
+	if (cub->game.player_c != 1)
+		exit_free_cub("Error: Missing player!", 1, cub);
 }
 
 void	cub_map_error(t_cub *cub)
